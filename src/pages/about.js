d6e3e0a90adf45fa865ab1cb457e07d137319dd7
MJTMace <17456158+MJTMace@users.noreply.github.com>
2020-08-24 17:40:22 +0100

    Fixed about photo width

--- src/pages/about.js
+++ src/pages/about.js
@@ -36,7 +36,7 @@ export const query = graphql`
       title
       subtitle
       photo {
-        fluid(maxWidth: 100, imgixParams: { fm: "jpg", auto: "compress" }) {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
         }
       }
