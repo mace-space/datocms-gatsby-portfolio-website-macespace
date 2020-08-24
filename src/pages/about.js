022d14feff3e2f6e5ad3c0c29d1d770c81013c6a
MJTMace <17456158+MJTMace@users.noreply.github.com>
2020-08-24 16:51:48 +0100

    changed max width for About Me image

--- src/pages/about.js
+++ src/pages/about.js
@@ -36,7 +36,7 @@ export const query = graphql`
       title
       subtitle
       photo {
-        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+        fluid(maxWidth: 200, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
         }
       }
