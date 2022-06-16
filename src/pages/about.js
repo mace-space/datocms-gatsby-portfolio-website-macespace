e5a0f02185670b5ebc0ebd6f53d492b1f14c6e5e
Mia <15634946+mace-space@users.noreply.github.com>
2022-06-16 01:24:40 +0100

    Revert maxWidth

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ export const query = graphql`
       title
       subtitle
       photo {
-        fluid(maxWidth: 1200, imgixParams: { fm: "jpg", auto: "compress" }) {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
         }
       }
